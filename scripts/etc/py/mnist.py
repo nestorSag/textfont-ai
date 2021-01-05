d43ff2f37272e599f49cc38bb88a3e5c3f026f94
nestorSag <nestor.sag@gmail.com>
2021-01-05 18:21:06 +0000

    upgraded to python 3.8 and added support for GPU

--- scripts/etc/py/mnist.py
+++ scripts/etc/py/mnist.py
@@ -1,7 +1,11 @@
 import numpy as np
 from tensorflow import keras
 from tensorflow.keras import layers
+import tensorflow as tf
 
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 num_classes = 10
 input_shape = (28,28,1)
