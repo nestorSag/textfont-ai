ac0bb16641231647c45b9a528ec54ab7e830614b
nestorSag <nestor.sag@gmail.com>
2021-01-20 15:04:38 +0000

    added feature to result analyzer

--- scripts/etc/py/evaluate-model.py
+++ scripts/etc/py/evaluate-model.py
@@ -67,17 +67,20 @@ class ValidationDataExaminer(object):
     self.index_df = pd.DataFrame({"predicted":predicted_labels,"true":true_labels,"score":predicted_scores,"index":list(range(len(predicted_scores)))})
     self.output_dir = f"{output_dir}/misclassified"
   #
-  def filter_model_errors(self,predicted_label=None,true_label=None,ascending_order=True,n=32):
+  def filter_model_results(self,predicted_label=None,true_label=None,ascending_order=True,n=32,misclassified_only=False):
     call_output_dir = f"{self.output_dir}/true_{true_label}_pred_{predicted_label}_{'asc' if ascending_order else 'desc'}"
     os.makedirs(call_output_dir)
     if predicted_label is None and true_label is None:
-      filtered_df = self.index_df[self.index_df]
+      filtered_df = self.index_df
     elif predicted_label is None and true_label is not None:
       filtered_df = self.index_df[self.index_df.true == true_label]
     elif true_label is None and predicted_label is not None:
       filtered_df = self.index_df[self.index_df.predicted == predicted_label]
     else:
       filtered_df = self.index_df[(self.index_df.predicted == predicted_label) & (self.index_df.true == true_label)]
+      #
+    if misclassified_only:
+      filtered_df = filtered_df[filtered_df.true != filtered_df.predicted]
     filtered_df = filtered_df.sort_values(by="score",ascending = ascending_order)
     #
     counter = 0
